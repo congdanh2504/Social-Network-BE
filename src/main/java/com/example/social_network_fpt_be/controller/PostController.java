bcace0ef160f29ed90e2b62de5ec0cf68af9f5ea
congdanh2504 <danhuchiha2002@gmail.com>
2022-07-14 14:46:57 +0700

    edit profile

--- src/main/java/com/example/social_network_fpt_be/controller/PostController.java
+++ src/main/java/com/example/social_network_fpt_be/controller/PostController.java
@@ -1,12 +1,9 @@
 package com.example.social_network_fpt_be.controller;
 
-import com.example.social_network_fpt_be.model.Image;
-import com.example.social_network_fpt_be.model.Post;
 import com.example.social_network_fpt_be.service.ImageService;
 import com.example.social_network_fpt_be.service.PostService;
 import com.example.social_network_fpt_be.service.UserService;
 import com.example.social_network_fpt_be.service.VideoService;
-import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +12,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.Objects;
-
-import static com.example.social_network_fpt_be.config.Setting.MAX_SIZE;
 
 @RestController
 @RequestMapping(path = "/api/v1/post")
@@ -48,25 +41,25 @@ public class PostController {
 
     @PostMapping(path = "", consumes = "multipart/form-data")
     public ResponseEntity<Object> createPost(@RequestPart("post_video") MultipartFile post_video,
-                                           @RequestPart("post_image") List<MultipartFile> post_image,
-                                           Authentication authentication,
-                                           @RequestParam("title") String title,
-                                           @RequestParam("description") String description) throws IOException {
+                                             @RequestPart("post_image") List<MultipartFile> post_image,
+                                             Authentication authentication,
+                                             @RequestParam("title") String title,
+                                             @RequestParam("description") String description) throws IOException {
         int id_user = userService.getUserByUsername(authentication.getName()).getId().intValue();
         if (post_video.getContentType() != null) {
             Hashtable<String, Object> checkVideo = videoService.checkFile(post_video);
-            if (checkVideo.get("status").equals(0)){
-                if (checkVideo.get("message").equals("Video file is empty")){
-                }else {
+            if (checkVideo.get("status").equals(0)) {
+                if (checkVideo.get("message").equals("Video file is empty")) {
+                } else {
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(checkVideo.get("message"));
                 }
             }
         }
-        if (post_image.get(0).getContentType() != null){
-            for (MultipartFile multipartFile: post_image){
+        if (post_image.get(0).getContentType() != null) {
+            for (MultipartFile multipartFile : post_image) {
                 Hashtable<String, Object> checkImage = imageService.checkFile(multipartFile);
-                if (checkImage.get("status").equals(0)){
-                    if (checkImage.get("message").equals("Image file is empty")){
+                if (checkImage.get("status").equals(0)) {
+                    if (checkImage.get("message").equals("Image file is empty")) {
                     } else {
                         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(checkImage.get("message"));
                     }
@@ -86,18 +79,18 @@ public class PostController {
         int id_user = userService.getUserByUsername(authentication.getName()).getId().intValue();
         if (post_video.getContentType() != null) {
             Hashtable<String, Object> checkVideo = videoService.checkFile(post_video);
-            if (checkVideo.get("status").equals(0)){
-                if (checkVideo.get("message").equals("Video file is empty")){
-                }else {
+            if (checkVideo.get("status").equals(0)) {
+                if (checkVideo.get("message").equals("Video file is empty")) {
+                } else {
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(checkVideo.get("message"));
                 }
             }
         }
-        if (post_image.get(0).getContentType() != null){
-            for (MultipartFile multipartFile: post_image){
+        if (post_image.get(0).getContentType() != null) {
+            for (MultipartFile multipartFile : post_image) {
                 Hashtable<String, Object> checkImage = imageService.checkFile(multipartFile);
-                if (checkImage.get("status").equals(0)){
-                    if (checkImage.get("message").equals("Image file is empty")){
+                if (checkImage.get("status").equals(0)) {
+                    if (checkImage.get("message").equals("Image file is empty")) {
                     } else {
                         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(checkImage.get("message"));
                     }
---
+++ src/main/java/com/example/social_network_fpt_be/util/Constraints.java
@@ -0,0 +1,7 @@
+package com.example.social_network_fpt_be.util;
+
+public class Constraints {
+    public static final String USER_COVER = "user_cover";
+    public static final String USER_AVT = "user_avt";
+    public static final String POST_IMAGE = "post_image";
+}
