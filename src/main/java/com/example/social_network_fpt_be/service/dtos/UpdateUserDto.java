202ce0a32357b4022ec4c5b66354a62c4f83685e
nqnphuong <ngnhatphuong.a.h@gmail.com>
2022-07-29 01:08:52 +0700

    follow

--- src/main/java/com/example/social_network_fpt_be/service/dtos/UpdateUserDto.java
+++ src/main/java/com/example/social_network_fpt_be/service/dtos/UpdateUserDto.java
@@ -1,4 +1,4 @@
-package com.example.social_network_fpt_be.service.dtos;
+package com.example.social_network_fpt_be.models.dtos;
 
 import lombok.Data;
 import org.springframework.format.annotation.NumberFormat;
--- src/main/java/com/example/social_network_fpt_be/service/dtos/UserDto.java
+++ src/main/java/com/example/social_network_fpt_be/service/dtos/UserDto.java
@@ -1,4 +1,4 @@
-package com.example.social_network_fpt_be.service.dtos;
+package com.example.social_network_fpt_be.models.dtos;
 
 import com.example.social_network_fpt_be.models.User;
 import lombok.AllArgsConstructor;
