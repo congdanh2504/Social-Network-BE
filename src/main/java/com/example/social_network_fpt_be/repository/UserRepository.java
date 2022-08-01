83612cbd89136b1f1d3c6614cf4f419ba490eed9
duong56 <91894323+duong56@users.noreply.github.com>
2022-08-01 19:04:02 +0700

    ok

--- src/main/java/com/example/social_network_fpt_be/repository/UserRepository.java
+++ src/main/java/com/example/social_network_fpt_be/repository/UserRepository.java
@@ -8,16 +8,7 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
-<<<<<<< HEAD
     @Query("SELECT u FROM User u WHERE function('dbo.ufn_removeMark', u.firstName) LIKE %:name% OR function('dbo.ufn_removeMark', u.lastName) LIKE %:name%")
-=======
-
-    @Query("SELECT u " +
-            "FROM User u " +
-            "WHERE function('dbo.ufn_removeMark', u.firstName) LIKE %:name% " +
-            "OR function('dbo.ufn_removeMark', u.lastName) LIKE %:name% " +
-            "OR u.username LIKE %:name%")
->>>>>>> 18cd36093c9660ea974902dfd34f347700be7dee
     List<User> searchByUsername(String name);
 
 }
